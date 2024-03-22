621a0bc05d8b4c2deacea41d809cc14c117f9665
Simone Manca <simone.manca.98@gmail.com>
2024-03-22 12:11:36 +0100

    creazione Classi Utente&Prestito

---
+++ src/main/java/simonemanca/entities/Prestito.java
@@ -0,0 +1,83 @@
+package simonemanca.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "prestiti")
+public class Prestito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id", nullable = false)
+    private Utente utente;
+
+    // Per semplicità, questo campo è commentato. Dovrai decidere come gestire il riferimento agli elementi prestati.
+    // @ManyToOne
+    // @JoinColumn(name = "elemento_id")
+    // private CatalogoItem elementoPrestato;
+
+    @Column(name = "data_inizio", nullable = false)
+    private LocalDate dataInizio;
+
+    @Column(name = "data_restituzione_prevista", nullable = false)
+    private LocalDate dataRestituzionePrevista;
+
+    @Column(name = "data_restituzione_effettiva")
+    private LocalDate dataRestituzioneEffettiva;
+
+    public Prestito() {
+    }
+
+    public Prestito(Utente utente, LocalDate dataInizio, LocalDate dataRestituzionePrevista, LocalDate dataRestituzioneEffettiva) {
+        this.utente = utente;
+        this.dataInizio = dataInizio;
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    // Metodo getter e setter per elementoPrestato omesso per semplicità.
+
+    public LocalDate getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(LocalDate dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public LocalDate getDataRestituzionePrevista() {
+        return dataRestituzionePrevista;
+    }
+
+    public void setDataRestituzionePrevista(LocalDate dataRestituzionePrevista) {
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+    }
+
+    public LocalDate getDataRestituzioneEffettiva() {
+        return dataRestituzioneEffettiva;
+    }
+
+    public void setDataRestituzioneEffettiva(LocalDate dataRestituzioneEffettiva) {
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+}
